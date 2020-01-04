26b3c72e668f4b9471eded5f5a66d378125222a3
Frederico Soares <soaresfred89@gmail.com>
2020-01-04 00:08:03 -0100

    global styles

--- src/pages/_layout/auth/styles.js
+++ src/pages/_layout/auth/styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: linear-gradient(-90deg, #274760, #62b5e7);
+  background: linear-gradient(-90deg, #f38c30, #f8cb58);
 `;
