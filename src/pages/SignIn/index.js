f6b88529a5335ba89b699a7611de7ca92ac8e909
Frederico Soares <soaresfred89@gmail.com>
2020-01-04 01:24:52 -0100

    styled authentication

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="ShopList" />
+
+      <form>
+        <input type="email" placeholder="Your email" />
+        <input type="password" placeholder="Secret password" />
+
+        <button type="submit">Log in</button>
+        <Link to="/register">Create a free account</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <>
+      <img src={logo} alt="ShopList" />
+
+      <form>
+        <input placeholder="Full name" />
+        <input type="email" placeholder="Your email" />
+        <input type="password" placeholder="Secret password" />
+
+        <button type="submit">Create your account</button>
+        <Link to="/">I already have an account</Link>
+      </form>
+    </>
+  );
 }
