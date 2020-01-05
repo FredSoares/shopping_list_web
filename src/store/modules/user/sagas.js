b4a379364e75c81a499ff83b4be2e7f51d945a65
Frederico Soares <soaresfred89@gmail.com>
2020-01-05 12:09:31 -0100

    update profile

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,35 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './action';
+
+export function* updateProfile({ payload }) {
+  try {
+    // get name email and the rest of other informations
+    const { name, email, ...rest } = payload.data;
+
+    // create profile object with name email and oldPasswod if exists
+    const profile = Object.assign(
+      { name, email },
+      rest.oldPassword ? rest : {}
+    );
+
+    // send request to update profile
+    const response = yield call(api.put, 'users', profile);
+
+    // show success message
+    toast.success('Perfil updated sucessfully!');
+
+    // call action update perfil success
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    // show error message
+    toast.error('Update failed check your data');
+
+    // call action update perfil failure
+    yield put(updateProfileFailure());
+  }
+}
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
