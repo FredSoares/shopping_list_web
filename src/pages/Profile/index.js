8b0cf8393eb4d230b7f838a4c464e662943aad25
Frederico Soares <soaresfred89@gmail.com>
2020-01-05 10:19:09 -0100

    profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your email" />
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Current password"
+        />
+        <Input name="newPassword" type="password" placeholder="New password" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm password"
+        />
+
+        <button type="submit">Update profile</button>
+      </Form>
+
+      <button type="button">Log Out</button>
+    </Container>
+  );
 }
